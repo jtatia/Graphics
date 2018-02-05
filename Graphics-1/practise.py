becb5fc6735ef59a01e29ebe5d8267bcbd266ab2
jtatia <jatjai40@gmail.com>
2018-02-06 02:31:43 +0530

    Initial Commit

---
+++ Graphics-1/practise.py
@@ -0,0 +1,46 @@
+import numpy as np
+import OpenGL
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+window = 0
+width, height = 500, 400
+
+def refresh2d(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, width, 0.0, height, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+
+def draw_rect(x, y, width, height):
+    glTranslatef(25,25,0)
+    glRotatef(x,0,0,1)
+    glScalef(2,2,0)
+    glBegin(GL_QUADS)
+    glVertex2f(x, y)
+    glVertex2f(x+width, y)
+    glVertex2f(x + width, y + height)
+    glVertex2f(x, y+height)
+    glEnd()
+
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    refresh2d(width,height)
+    glColor3f(0.0, 0.0, 1.0)
+    draw_rect(10,10,200,100)
+    glutSwapBuffers()
+
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+glutInitWindowSize(width, height)
+glutInitWindowPosition(0, 0)
+window = glutCreateWindow("Testing")
+glutDisplayFunc(draw)
+glutIdleFunc(draw)
+glutMainLoop()
+
